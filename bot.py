650d4e566cfef8c540a60f360d42b516614e8c3b
Ruslan <rusl.popov@gmail.com>
2025-04-05 11:02:31 +0300

    Updated bot to version 0.0.3

--- bot.py
+++ bot.py
@@ -12,12 +12,22 @@ print("Using token ...", bot_token[-5:])
 bot = telebot.TeleBot(bot_token)
 
 
+@bot.message_handler(commands=["start"])
+def handle_start(message: Message):
+    print("Получена команда /start")
+    username = message.from_user.full_name if message.from_user.full_name else message.from_user.username
+    text = f"""Здравствуйте {username}
+Я помогу вам изучать иностранные слова
+Версия бота: 0.0.3
+"""
+    bot.send_message(message.chat.id, text)
+
+
 @bot.message_handler()
 def handle_message(message: Message):
     info = f"""Вы написали: `{message.text}`
 Пользователь: `{message.from_user.username}`
 Чат: `{message.chat.id}`
-Версия бота: 0.0.2
 """
     print(info)
     bot.send_message(message.chat.id, info, parse_mode="Markdown")
