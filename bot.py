e7876c5af192b02586882bb8d60859897ed97cbe
Ruslan <rusl.popov@gmail.com>
2025-05-05 11:08:11 +0300

    User sessions persistence

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-# @PyBotWords_Bot
-
-
 import telebot
 from telebot.types import Message
 from os import getenv
@@ -10,17 +7,27 @@ from random import randint, sample
 import dict_general
 import dict_econ
 import dict_pdb
+import json
 
 
 load_dotenv()
 bot_token = getenv("TOKEN")
 print("Using token ...", bot_token[-5:])
 
+with open("user_sessions.json", "rt", encoding="UTF-8") as f:
+    user_sessions = json.load(f)
+
+print(user_sessions)
+
 dict_list = {
     "English General": ("/dict_eng", dict_general.words_dict),
     "English Economics": ("/dict_enec", dict_econ.words_dict),
     "Python Databases": ("/dict_pdb", dict_pdb.words_dict),
 }
+
+
+default_dict = "/dict_eng"
+
 
 def dict_info():
     info_list = []
@@ -30,26 +37,49 @@ def dict_info():
         info_list.append(f"{sel_icon} {command} - {name} ({len(dict)} слов)")
     return "\n".join(info_list)
 
-words_dict, word_codes, selected_dict = None, None, None
+
+words_dict, selected_dict = None, None
+
 
 def load_dict(command):
-    key, dict = next(((k, d) for k, (cmd, d) in dict_list.items() if cmd == command), None)
+    name, dict = next(
+        ((k, d) for k, (cmd, d) in dict_list.items() if cmd == command), None
+    )
     global selected_dict
     selected_dict = command
     global words_dict
     words_dict = dict
-    global word_codes
-    word_codes = list(words_dict.keys())
-    return key
+    print(f"Загружен словарь {command}, {selected_dict=} {len(words_dict)=}")
+    return name
 
-load_dict("/dict_eng")
 
-user_sessions = {}
+user_session = None
+
+
+def load_session(user_id):
+    global user_session
+    user_session = user_sessions.get(str(user_id))
+    print("Найдена сессия", user_session)
+    if user_session:
+        dict_name = user_session[1]
+        load_dict(dict_name)
+    else:
+        load_dict(default_dict)
+        user_session = [None, default_dict]
+        user_sessions[user_id] = user_session
+
+
+def save_sessions():
+    with open("user_sessions.json", "wt", encoding="UTF-8") as f:
+        json.dump(user_sessions, f, ensure_ascii=False, indent=2)
+
+
+
 bot = telebot.TeleBot(bot_token)
 
 
 def send_word(message: Message):
-    random4 = sample(word_codes, 4)
+    random4 = sample(list(words_dict.keys()), 4)
     # while len(random4) < 4:
     #     index = randint(0, len(word_codes) - 1)
     #     new_pair = words_dict[inde]
@@ -78,10 +108,11 @@ def send_word(message: Message):
         chat_id=message.chat.id,
         text=f"Выбери перевод слова:\n\n**{english_word.upper()}**\n\nПример использования:\n\n{usage_example}",
         reply_markup=keyboard,
-        parse_mode="Markdown"
+        parse_mode="Markdown",
     )
 
-    user_sessions[message.from_user.id] = russian_word
+    user_sessions[message.from_user.id] = [russian_word, selected_dict]
+    print(user_sessions)
 
 
 @bot.message_handler(commands=["start"])
@@ -89,6 +120,9 @@ def handle_start(message: Message):
     print(
         f"Получена команда /start от пользователя {message.from_user.id} {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}"
     )
+
+    load_session(message.from_user.id)
+
     username = (
         message.from_user.full_name
         if message.from_user.full_name
@@ -113,12 +147,13 @@ def handle_message(message: Message):
         f"Получено сообщение от пользователя {message.from_user.id} {message.from_user.username} {message.from_user.first_name} {message.from_user.last_name}"
     )
     print(message.text)
+    load_session(message.from_user.id)
 
     if message.text and message.text.startswith("/"):
         handle_command(message)
     else:
         word = message.text
-        correct_word = user_sessions[message.from_user.id]
+        correct_word = user_sessions[message.from_user.id][0]
         if word == correct_word:
             text = "Верно! Переходим к следующему слову"
             bot.send_message(message.chat.id, text)
@@ -134,7 +169,7 @@ def handle_command(message: Message):
         name = load_dict(command)
         bot.send_message(message.chat.id, "Загружен словарь " + name)
         send_word(message)
-    
+
 
 def print_info(message):
     info = f"""Вы написали: `{message.text}`
@@ -146,3 +181,5 @@ def print_info(message):
 
 
 bot.infinity_polling()
+
+save_sessions()
